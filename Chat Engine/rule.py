2f553a3e3edb10e834c45fd77c2e229eadef9f8b
Grant Dunbar <grantdunbar8@gmail.com>
2022-04-01 16:55:07 -0600

    Project4-02: Updated file

--- Chat Engine/rule.py
+++ Chat Engine/rule.py
@@ -1,11 +1,22 @@
 class rule:
-    def __init__(self, type, input, output, children):
+    def __init__(self, type, input, output):
         self.type = type
         self.input = input
         self.output = output
-        self.children = children
+        self.children = []
 
     def print(self):
         print(self.type + " " + self.input + " ")
         for item in self.output:
             print(item)
+    
+    def getLastChild(self):
+        return self.children[len(self.children)-1]
+
+    def addChild(self, rule):
+        self.children.append(rule)
+
+    def printChildren(self):
+        for child in self.children:
+            print("Children: " + child.type)
+            child.printChildren()
--- Chat Engine/runner.py
+++ Chat Engine/runner.py
@@ -1,4 +1,3 @@
-from pickle import FALSE, TRUE
 from rule import *
 
 file = open('testing1.txt', 'r')
@@ -7,51 +6,71 @@ lines = file.readlines()
 
 rules = []
 type=''
+childCount = 0
 for line in lines:
-    print("LINE: " + line)
     
 
     if '#' not in line[0]:
-        if len(type) > 0 and type.split('u')[1] < line.split(':')[0].replace(' ', '').split('u')[1]:
-            print('CHILD')
+        # if len(type) > 0 and type.split('u')[1] < line.split(':')[0].replace(' ', '').split('u')[1]:
+        #     #print("TYPE: " + type.split('u')[1] + " | LINE: " + line.split(':')[0].replace(' ', '').split('u')[1])
+        #     print('CHILD')
+            
+        # elif len(type) > 0 and type.split('u')[1] > line.split(':')[0].replace(' ', '').split('u')[1] and len(line.split(':')[0].replace(' ', '').split('u')[0]) is not NULL: 
+        #     print("LINE2: " + line.split(':')[0].replace(' ', '').split('u')[0] + ":")
+        #     print("CHILD")
         type = line.split(':')[0].replace(' ', '')
-        input = line.split(':')[1].split('(')[1].split(')')[0]
-        print(len(input))
-        if len(line.split(':')) > 2:
-            output = []
-            if '[' not in line.split(':')[2]:
-                if '\"' in line.split(':')[2]:
-                    output.append(line.split(':')[2].split('\"')[1])
+        if(type == 'u'):
+            print("LINE: " + line)
+
+            
+            input = line.split(':')[1].split('(')[1].split(')')[0]
+            if len(line.split(':')) > 2:
+                output = []
+                if '[' not in line.split(':')[2]:
+                    if '\"' in line.split(':')[2]:
+                        output.append(line.split(':')[2].split('\"')[1])
+                    else:
+                        output.append(line.split(':')[2])
                 else:
-                    output.append(line.split(':')[2])
+                    outputLine = line.split(':')[2].split('[')[1].split(']')[0]
+                    print("LINE HERE: " + outputLine)
+                    isQuote = False
+                    strBuilder = ''
+                    for element in range(0, len(outputLine)):
+                        print(outputLine[element])
+                        if outputLine[element] != ' ' and outputLine[element] != '\"' and isQuote == False:
+                            strBuilder += outputLine[element]
+                        elif outputLine[element] == ' ' and isQuote == False and len(strBuilder) > 0:
+                            output.append(strBuilder)
+                            print("BUILD :  " + strBuilder)
+                            strBuilder = ''
+                        elif outputLine[element] == '\"' and isQuote == False:
+                            isQuote = True
+                        elif outputLine[element] == '\"' and isQuote == True:
+                            output.append(strBuilder)
+                            strBuilder = ''
+                            isQuote = False
+                        elif outputLine[element] != '\"' and isQuote == True:
+                            strBuilder += outputLine[element]
+                    output.append(strBuilder)
+                rules.append(rule(type, input, output))
             else:
-                outputLine = line.split(':')[2].split('[')[1].split(']')[0]
-                print("LINE HERE: " + outputLine)
-                isQuote = FALSE
-                strBuilder = ''
-                for element in range(0, len(outputLine)):
-                    print(outputLine[element])
-                    if outputLine[element] != ' ' and outputLine[element] != '\"' and isQuote == FALSE:
-                        strBuilder += outputLine[element]
-                    elif outputLine[element] == ' ' and isQuote == FALSE and len(strBuilder) > 0:
-                        output.append(strBuilder)
-                        print("BUILD :  " + strBuilder)
-                        strBuilder = ''
-                    elif outputLine[element] == '\"' and isQuote == FALSE:
-                        isQuote = TRUE
-                    elif outputLine[element] == '\"' and isQuote == TRUE:
-                        output.append(strBuilder)
-                        strBuilder = ''
-                        isQuote = FALSE
-                    elif outputLine[element] != '\"' and isQuote == TRUE:
-                        strBuilder += outputLine[element]
-                output.append(strBuilder)
-            rules.append(rule(type, input, output, 'lit'))
+                print('ERROR in this line')
         else:
-            print('ERROR in this line')
+            if len(line.split(':')) > 2:
+                childCount = int(type.split('u')[1])
+                output = 'lit'
+                childOfRule = rules[len(rules)-1]
+                for x in range (1, childCount):
+                    childOfRule = childOfRule.getLastChild()
+                
+                childOfRule.addChild(rule(type, input, output))
+            else:
+               print('ERROR in this line') 
         
         
 for item in rules:
     item.print()
+    item.printChildren() 
 
 file.close()
