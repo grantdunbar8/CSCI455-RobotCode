ea23c589af7d99533f4bca4b66e4a6165a4df55b
Gregory-Hill <tkddudebro@gmail.com>
2022-04-21 19:23:13 -0600

    Update 3
    All sliders work, slider doesn't reset

--- GUI_Project/RobotGUI.py
+++ GUI_Project/RobotGUI.py
@@ -29,7 +29,7 @@ BoxLayout:
 
 class MyApp(App):
 
-    def makeDropDown(self, dropdown, popup):
+    def makeDropDown(self, dropdown, popup, popup2):
         for i in range(7):
             if i == 0:       
                 ddButton = Button(text = "Robot Move", size_hint_y = None)
@@ -39,19 +39,22 @@ class MyApp(App):
             elif i == 1:
                 ddButton = Button(text = "Robot Turn", size_hint_y = None)
                 ddButton.bind(on_release = lambda ddButton: dropdown.select(ddButton.text))
-
+                ddButton.bind(on_release = popup2.open)
                 dropdown.add_widget(ddButton)
             elif i == 2:
                 ddButton = Button(text = "Head Tilt", size_hint_y = None)
                 ddButton.bind(on_release = lambda ddButton: dropdown.select(ddButton.text))
+                ddButton.bind(on_release = popup2.open)
                 dropdown.add_widget(ddButton)
             elif i == 3:
                 ddButton = Button(text = "Head Pan", size_hint_y = None)
                 ddButton.bind(on_release = lambda ddButton: dropdown.select(ddButton.text))
+                ddButton.bind(on_release = popup2.open)
                 dropdown.add_widget(ddButton)
             elif i == 4:
                 ddButton = Button(text = "Waist Turn", size_hint_y = None)
                 ddButton.bind(on_release = lambda ddButton: dropdown.select(ddButton.text))
+                ddButton.bind(on_release = popup2.open)
                 dropdown.add_widget(ddButton)
             elif i == 5:
                 ddButton = Button(text = "Human Talk", size_hint_y = None)
@@ -65,30 +68,71 @@ class MyApp(App):
 
     ############make update only update the button you want to - good I think
     def update(self, event):
-        if ('seconds' not in self.button1.text) and ('Action1' in self.button1.text):
-            newText = self.button1.text + " for: " + str(self.testVal) + " seconds"
-            self.button1.text = newText
-        elif ('seconds' not in self.button2.text) and ('Action2' in self.button2.text):
-            newText = self.button2.text + " for: " + str(self.testVal) + " seconds"
-            self.button2.text = newText
-        elif ('seconds' not in self.button3.text) and ('Action3' in self.button3.text):
-            newText = self.button3.text + " for: " + str(self.testVal) + " seconds"
-            self.button3.text = newText
-        elif ('seconds' not in self.button4.text) and ('Action4' in self.button4.text):
-            newText = self.button4.text + " for: " + str(self.testVal) + " seconds"
-            self.button4.text = newText
-        elif ('seconds' not in self.button5.text) and ('Action5' in self.button5.text):
-            newText = self.button5.text + " for: " + str(self.testVal) + " seconds"
-            self.button5.text = newText
-        elif ('seconds' not in self.button6.text) and ('Action6' in self.button6.text):
-            newText = self.button6.text + " for: " + str(self.testVal) + " seconds"
-            self.button6.text = newText
-        elif ('seconds' not in self.button7.text) and ('Action7' in self.button7.text):
-            newText = self.button7.text + " for: " + str(self.testVal) + " seconds"
-            self.button7.text = newText
-        elif ('seconds' not in self.button8.text) and ('Action8' in self.button8.text):
-            newText = self.button8.text + " for: " + str(self.testVal) + " seconds"
-            self.button8.text = newText
+
+        if ('Action1' in self.button1.text):
+            if ('seconds' not in self.button1.text) and ('Robot Move' in self.button1.text):
+                newText = self.button1.text + " for: " + str(self.testVal) + " seconds"
+                self.button1.text = newText
+            elif ('plus' not in self.button1.text) and (('Robot Turn' in self.button1.text) or ('Head Tilt' in self.button1.text) or ('Head Pan' in self.button1.text) or ('Waist Turn' in self.button1.text)):
+                newText = self.button1.text + " plus: " + str(self.testVal)
+                self.button1.text = newText
+
+        if ('Action2' in self.button2.text):
+            if ('seconds' not in self.button2.text) and ('Robot Move' in self.button2.text):
+                newText = self.button2.text + " for: " + str(self.testVal) + " seconds"
+                self.button2.text = newText
+            elif ('plus' not in self.button2.text) and (('Robot Turn' in self.button2.text) or ('Head Tilt' in self.button2.text) or ('Head Pan' in self.button2.text) or ('Waist Turn' in self.button2.text)):
+                newText = self.button2.text + " plus: " + str(self.testVal)
+                self.button2.text = newText
+
+        if ('Action3' in self.button3.text):
+            if ('seconds' not in self.button3.text) and ('Robot Move' in self.button3.text):
+                newText = self.button3.text + " for: " + str(self.testVal) + " seconds"
+                self.button3.text = newText
+            elif ('plus' not in self.button3.text) and (('Robot Turn' in self.button3.text) or ('Head Tilt' in self.button3.text) or ('Head Pan' in self.button3.text) or ('Waist Turn' in self.button3.text)):
+                newText = self.button3.text + " plus: " + str(self.testVal)
+                self.button3.text = newText
+
+        if ('Action4' in self.button4.text):
+            if ('seconds' not in self.button4.text) and ('Robot Move' in self.button4.text):
+                newText = self.button4.text + " for: " + str(self.testVal) + " seconds"
+                self.button4.text = newText
+            elif ('plus' not in self.button4.text) and (('Robot Turn' in self.button4.text) or ('Head Tilt' in self.button4.text) or ('Head Pan' in self.button4.text) or ('Waist Turn' in self.button4.text)):
+                newText = self.button4.text + " plus: " + str(self.testVal)
+                self.button4.text = newText
+
+        if ('Action5' in self.button5.text):
+            if ('seconds' not in self.button5.text) and ('Robot Move' in self.button5.text):
+                newText = self.button5.text + " for: " + str(self.testVal) + " seconds"
+                self.button5.text = newText
+            elif ('plus' not in self.button5.text) and (('Robot Turn' in self.button5.text) or ('Head Tilt' in self.button5.text) or ('Head Pan' in self.button5.text) or ('Waist Turn' in self.button5.text)):
+                newText = self.button5.text + " plus: " + str(self.testVal)
+                self.button5.text = newText
+
+        if ('Action6' in self.button6.text):
+            if ('seconds' not in self.button6.text) and ('Robot Move' in self.button6.text):
+                newText = self.button6.text + " for: " + str(self.testVal) + " seconds"
+                self.button6.text = newText
+            elif ('plus' not in self.button6.text) and (('Robot Turn' in self.button6.text) or ('Head Tilt' in self.button6.text) or ('Head Pan' in self.button6.text) or ('Waist Turn' in self.button6.text)):
+                newText = self.button6.text + " plus: " + str(self.testVal)
+                self.button6.text = newText
+
+        if ('Action7' in self.button7.text):
+            if ('seconds' not in self.button7.text) and ('Robot Move' in self.button7.text):
+                newText = self.button7.text + " for: " + str(self.testVal) + " seconds"
+                self.button7.text = newText
+            elif ('plus' not in self.button7.text) and (('Robot Turn' in self.button7.text) or ('Head Tilt' in self.button7.text) or ('Head Pan' in self.button7.text) or ('Waist Turn' in self.button7.text)):
+                newText = self.button7.text + " plus: " + str(self.testVal)
+                self.button7.text = newText
+
+        if ('Action8' in self.button8.text):
+            if ('seconds' not in self.button8.text) and ('Robot Move' in self.button8.text):
+                newText = self.button8.text + " for: " + str(self.testVal) + " seconds"
+                self.button8.text = newText
+            elif ('plus' not in self.button8.text) and (('Robot Turn' in self.button8.text) or ('Head Tilt' in self.button8.text) or ('Head Pan' in self.button8.text) or ('Waist Turn' in self.button8.text)):
+                newText = self.button8.text + " plus: " + str(self.testVal)
+                self.button8.text = newText
+
 
     def build(self):
 
@@ -112,16 +156,18 @@ class MyApp(App):
         dropdown8 = DropDown()
 
         self.testVal = ''
+
+
         def OnSliderValueChange(instance, value):
-            #label1.text = str(value) + " seconds"
+            label1.text = str(value) + " seconds"
             self.testVal = value
-            print(value)
-            print("HERE " + str(self.testVal))
-            print(instance)
-            return value
+            # print(value)
+            # print("HERE " + str(self.testVal))
+            # print(instance)
+            #return value
 
         slider = Slider(
-            min = 0, 
+            min = -10, 
             max = 10, 
             orientation = 'horizontal',
             value_track = True,
@@ -129,30 +175,56 @@ class MyApp(App):
             step = 1
             
             )
-        #label1 = Label()
+       
+        label1 = Label()
         popupButton = Button(text = "Set Value")
-        
-
         slider.bind(value=OnSliderValueChange)
         box = BoxLayout()
         box.add_widget(slider)
-        #box.add_widget(label1)
+        box.add_widget(label1)
         box.add_widget(popupButton)
-
-        popup = Popup(title = "ROBOT MOVE WINDOW", content = box, size_hint = (None, None), size = (400, 100))
-        #popup2 = Popup(title = "ROBOT MOVE WINDOW", content = box, size_hint = (None, None), size = (400, 100))
+        popup = Popup(title = "+ = forward | - = backward", content = box, size_hint = (None, None), size = (400, 100))
         popupButton.bind(on_press = popup.dismiss)
         popupButton.bind(on_press = self.update)
 
+#################################################
+        def OnSliderValueChange2(instance, value):
+            label2.text = str(value) 
+            self.testVal = value
+            # print(value)
+            # print("HERE " + str(self.testVal))
+            # print(instance)
+            #return value
 
-        self.makeDropDown(dropdown, popup)
-        self.makeDropDown(dropdown2, popup)
-        self.makeDropDown(dropdown3, popup)
-        self.makeDropDown(dropdown4, popup)
-        self.makeDropDown(dropdown5, popup)
-        self.makeDropDown(dropdown6, popup)
-        self.makeDropDown(dropdown7, popup)
-        self.makeDropDown(dropdown8, popup)
+        slider2 = Slider(
+            min = -1500, 
+            max = 1500, 
+            orientation = 'horizontal',
+            value_track = True,
+            value_track_color = [1, 0, 0, 1],
+            step = 100
+            
+            )
+        label2 = Label()
+        popupButton2 = Button(text = "Set Value")
+        slider2.bind(value=OnSliderValueChange2)
+        box2 = BoxLayout()
+        box2.add_widget(slider2)
+        box2.add_widget(label2)
+        box2.add_widget(popupButton2)
+        popup2 = Popup(title = "+ = up, right | - = down, left", content = box2, size_hint = (None, None), size = (400, 100))
+        popupButton2.bind(on_press = popup2.dismiss)
+        popupButton2.bind(on_press = self.update)
+
+
+        self.makeDropDown(dropdown, popup, popup2)
+        self.makeDropDown(dropdown2, popup, popup2)
+        self.makeDropDown(dropdown3, popup, popup2)
+        self.makeDropDown(dropdown4, popup, popup2)
+        self.makeDropDown(dropdown5, popup, popup2)
+        self.makeDropDown(dropdown6, popup, popup2)
+        self.makeDropDown(dropdown7, popup, popup2)
+        self.makeDropDown(dropdown8, popup, popup2)
 
 
         self.button1 = Button(text = "Action 1")
