b12b21358dfe1a47fe409c0032b2fb811713cff0
LordThesus <70674757+LordThesus@users.noreply.github.com>
2022-04-21 19:00:37 -0600

    Command.py reduced variables

--- GUI_Project/Command.py
+++ GUI_Project/Command.py
@@ -1,25 +1,21 @@
 from Methods import *
 #import serial
 class Command:
-    exeDuration = 0
+    exeValue = 0
     exeType = ''
-    exeAngle = 0
     methods = ''
     
-    def __init__(self, inTime, inType, inAngle):
+    def __init__(self, inType, inValue):
         # Duration the movement will be executed
-        self.exeTime = inTime
+        self.exeValue = inValue
         
         # Type of motion that will be executed
         self.exeType = inType
         
-        # Angle for rotation or movement or string
-        self.exeAngle = inAngle
         usb = serial.Serial('/dev/ttyACM0')
         print (usb.name)
         print(usb.baudrate)
         self.methods = Methods(usb)
-
 
     def ExecuteCommand(self):
         # exeType can be integers or strings or any type of variable
@@ -29,12 +25,12 @@ class Command:
             if(self.exeDuration > 0):
                 print("TYPE: move forward")
                 self.methods.positionMotor(0, 6000, 0)
-                self.methods.positionMotor(0, 4500, self.exeDuration)
+                self.methods.positionMotor(0, 4500, int(self.exeValue))
                 self.methods.positionMotor(0, 6000, 0)
             else:
                 print("TYPE: move backward")
                 self.methods.positionMotor(0, 6000, 0)
-                self.methods.positionMotor(0, 7500, abs(self.exeDuration))
+                self.methods.positionMotor(0, 7500, abs(int(self.exeValue)))
                 self.methods.positionMotor(0, 6000, 0)
 
         elif len(self.exeType) == len('turn right') and self.exeType == 'turn right':
@@ -49,24 +45,24 @@ class Command:
 
         elif len(self.exeType) == len('head tilt') and self.exeType == 'head tilt':
             print("TYPE: head tilt")
-            self.methods.positionMotor(4, 6000 + self.exeAngle, 0)
+            self.methods.positionMotor(4, 6000 + int(self.exeValue), 0)
 
         elif len(self.exeType) == len('head pan') and self.exeType == 'head pan':
             print("TYPE: head pan")
-            self.methods.positionMotor(3, 6000 - self.exeAngle, 0)
+            self.methods.positionMotor(3, 6000 - int(self.exeValue), 0)
 
         elif len(self.exeType) == len('waist') and self.exeType == 'waist':
             print("TYPE: waist")
-            self.methods.positionMotor(2, 6000 - self.exeAngle, 0)
+            self.methods.positionMotor(2, 6000 - int(self.exeValue), 0)
 
         elif len(self.exeType) == len('pause') and self.exeType == 'pause':
             print("TYPE: pause")
-            self.methods.positionMotor(100, 0, self.exeDuration)
+            self.methods.positionMotor(100, 0, int(self.exeValue))
 
         elif len(self.exeType) == len('speak') and self.exeType == 'speak':
             print('TYPE: speak')
-            self.methods.speak(self.exeAngle)
+            self.methods.speak(self.exeValue)
 
         elif len(self.exeType) == len('wait for') and self.exeType == 'wait for':
-            print('TYPE: wait for ' + self.exeAngle)
-            self.methods.waitForCommand(self.exeAngle)
+            print('TYPE: wait for ' + self.exeValue)
+            self.methods.waitForCommand(self.exeValue)
