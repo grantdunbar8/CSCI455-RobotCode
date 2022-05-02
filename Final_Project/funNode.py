3d0c28d545b0dbc0aaa1ca4f2ea749864df5b7ad
Gregory-Hill <tkddudebro@gmail.com>
2022-05-01 19:39:21 -0600

    fun node move fix

--- Final_Project/funNode.py
+++ Final_Project/funNode.py
@@ -19,14 +19,14 @@ class FunNode:
             self.sound.CreateSound("hpBoost.mp3", "You got an HP boost!")
             self.sound.PlaySound("hpBoost.mp3")
             player.boostHealth()
-            player.mover.boostMove()
+            #player.mover.boostMove()
             self.beenVisited = True
         elif(not self.beenVisited):
             print('attack boost')
             self.sound.CreateSound("attBoost.mp3", "You got and attack power boost!")
             self.sound.PlaySound("attBoost.mp3")
             player.boostAttack()
-            player.mover.boostMove()
+            #player.mover.boostMove()
             self.beenVisited = True
         else:
             print('you have already visited this fun node')
