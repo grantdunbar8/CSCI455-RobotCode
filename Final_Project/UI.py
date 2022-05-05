45f7b073229fe8d981f871be2d61b12e0190a278
LordThesus <70674757+LordThesus@users.noreply.github.com>
2022-05-05 13:06:56 -0600

    Updated UI.py

--- Final_Project/UI.py
+++ Final_Project/UI.py
@@ -1,24 +1,40 @@
 from tkinter import *
 from tkinter.ttk import *
 import _thread, threading
+from PIL import ImageTk, Image
 
 master = Tk()
+img = None
 
 class UI:
     def __init__(self):
-        print("init")
+        print("init: UI")
         
     def CreateUI(self):
         print("HERE")
 
-        master.geometry("200x200")
-        label = Label(master, text="Test")
-        label.pack(pady=10)
+        master.geometry("400x400")
+
+        global img
+        img = ImageTk.PhotoImage(Image.open("face1.png"))
+        canvas.create_image(20,20, anchor=NW, image=img)
 
     def OtherMethod(self):
-        for i in range(0, 100):
+        for i in range(0, 10):
             print(i)
         
+class ChangeUI:
+    def __init__(self):
+        print("init: ChangeUI")
+        
+    def ChangeImage(self, fileName):
+        global img
+        img = ImageTk.PhotoImage(Image.open(fileName))
+        canvas.create_image(20,20, anchor=NW, image=img)
+
+canvas = Canvas(master, width=300, height=300)
+canvas.pack()
+
 inst = UI()
 
 try:
